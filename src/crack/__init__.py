48c66f8a5b3d14a9359417d86cc8e17a39d62bca
Royaltyprogram <165759805+Royaltyprogram@users.noreply.github.com>
2026-05-09 12:56:11 +0900

    Revert "Add markdown workflow router foundation"
    This reverts commit faa2de7a1ed4fea9e2f49dda43624a8ca613f31c.

--- src/crack/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Markdown-backed Codex workflow orchestration primitives."""
-
-from .router import RouteDecision, Router
-from .state import MarkdownState
-
-__all__ = ["MarkdownState", "RouteDecision", "Router"]
